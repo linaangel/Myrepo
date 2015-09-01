12eb7cea21c9b5c837831dba497d273db5900efc
Lina Angel <LinaAngel@MacBook-Pro-de-MAC.local>
2015-09-01 18:13:13 -0400

    Your Update Message

---
+++ Class2/empty-example/sketch.js
@@ -0,0 +1,13 @@
+function alert2(saywhat) {
+	alert(saywhat);
+}
+
+function randomletterfromtext(text) {
+	var letterNumber = random(text.length);
+	var letter = text.charAt(letterNumber);
+	return letter;
+}
+
+function random(text) {
+	return text;
+}
